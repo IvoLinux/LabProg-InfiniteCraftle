86807e78ac21477749dd8256a7b188d5b85d1641
varad-comrad <fabricio_asfora_002@hotmail.com>
2024-04-17 09:02:16 -0300

    Debugged ICModel. It cannot contain newlines

--- src/main/java/com/example/infinite/ai/ICModel.java
+++ src/main/java/com/example/infinite/ai/ICModel.java
@@ -5,15 +5,14 @@ public class ICModel extends ModelBase {
     private String zeroShotLearningTemplate;
     private int counter = 0;
 
-
     public ICModel(String url, String apiKey, int maxTokens, float temperature) {
         super(url, apiKey, maxTokens, temperature);
-        zeroShotLearningTemplate = "What is the answer that best fits the operands? Choose only one answer.\n\n";
+        zeroShotLearningTemplate = "What is the answer that best fits the operands? Choose only one answer: ";
     }
 
     public void updateTemplate(String operand1, String operand2, String answer){
         if(counter < 5){
-            zeroShotLearningTemplate += String.format("%d. %s + %s = %s\n", counter+1, operand1, operand2, answer);
+            zeroShotLearningTemplate += String.format("%s + %s = %s; ", operand1, operand2, answer);
             counter++;
         }
 
@@ -26,6 +25,7 @@ public class ICModel extends ModelBase {
 
     public String retrieveAnswer(String op1, String op2){
         String question = addQuestion(op1, op2);
-        return retrieveAnswer(question);
+        System.out.println(question);
+        return super.retrieveAnswer(question);
     }
 }
